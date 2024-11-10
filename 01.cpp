d871cf3925f67c21d8e311dcd7d4ee52319cdd5f
1379 <18510805232@163.com>
2024-11-10 14:00:35 +0800

    01

---
+++ 01.cpp
@@ -0,0 +1,23 @@
+//#define _CRT_SECURE_NO_WARNINGS 1
+#include <conio.h>
+#include <iostream>
+#include <string.h>
+using namespace std;
+
+
+int* fun()
+{
+	int* p = new int(100);
+	return p;
+}
+
+
+int main()
+{
+	
+	int* p=fun();
+	cout << *p;
+
+
+	return 0;
+}
